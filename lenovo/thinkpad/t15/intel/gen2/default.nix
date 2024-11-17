54285b8e9836e16d2560897c7f3ff311e584c5dd
Omegaice <950526+Omegaice@users.noreply.github.com>
2024-11-17 16:40:25 -0500

    fix

--- lenovo/thinkpad/t15/intel/gen2/default.nix
+++ lenovo/thinkpad/t15/intel/gen2/default.nix
@@ -6,5 +6,10 @@
     ../../../../../common/gpu/nvidia/ampere
     ../../../../../common/gpu/nvidia/prime-sync.nix
   ];
+
+  hardware.nvidia.prime = {
+		intelBusId = "PCI:0:2:0";
+		nvidiaBusId = "PCI:1:0:0";
+  };
 }
 
